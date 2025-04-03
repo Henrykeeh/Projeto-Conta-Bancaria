3705342e9705da2561083e88cab8ff0940882447
Henrykeeh <c.henrique.silvabarbosa@gmail.com>
2025-04-02 21:22:59 -0300

    feat: acrescentado o método auxiliar buscarNaCollection na classe ContaController e completado os métodos atualizar, deletar e procurarPorNumero.

--- src/conta/controller/ContaController.java
+++ src/conta/controller/ContaController.java
@@ -12,7 +12,15 @@ public class ContaController implements ContaRepository {
 
 	@Override
 	public void procurarPorNumero(int numero) {
+		var conta = buscarNaCollection(numero);
 
+		if (conta != null) {
+			conta.visualizar();
+		}
+
+		else {
+			System.out.println("A conta número: " + numero + " não foi encontrada!");
+		}
 	}
 
 	@Override
@@ -32,12 +40,33 @@ public class ContaController implements ContaRepository {
 
 	@Override
 	public void atualizar(Conta conta) {
+		var buscaConta = buscarNaCollection(conta.getNumero());
+
+		if (buscaConta != null) {
+			listaContas.set(listaContas.indexOf(buscaConta), conta);
+
+			System.out.println("\nA conta número: " + conta.getNumero() + " foi atualizada com sucesso!");
+		}
+
+		else {
+			System.out.println("\nA conta número: " + conta.getNumero() + " não foi encontrada!");
+		}
 
 	}
 
 	@Override
 	public void deletar(int numero) {
+		var conta = buscarNaCollection(numero);
 
+		if (conta != null) {
+			if (listaContas.remove(conta) == true) {
+				System.out.println("A conta número: " + numero + " foi removida com sucesso!");
+			}
+
+		} else {
+			System.out.println("A conta número: " + numero + " não foi encontrada!");			
+		}
+		
 	}
 
 	@Override
@@ -57,5 +86,16 @@ public class ContaController implements ContaRepository {
 
 	public int gerarNumero() {
 		return ++numero;
+	}
+
+	public Conta buscarNaCollection(int numero) {
+		for (var conta : listaContas) {
+			if (conta.getNumero() == numero) {
+
+				return conta;
+			}
+		}
+
+		return null;
 	}
 }
